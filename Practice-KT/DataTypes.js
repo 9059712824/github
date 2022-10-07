5455cb749840fdb0b800e182dc94af6c69868b23
9059712824 <rvkrishna13052001@gmail.com>
2022-10-07 15:59:41 +0530

    Day1

---
+++ Practice-KT/DataTypes.js
@@ -0,0 +1,31 @@
+let data=8          //Number
+let user="venky"    //String
+data="venky"
+console.log(data)
+console.log(typeof data)
+
+//Number
+let num=68461168
+console.log(num*8978)
+console.log(typeof num)
+
+//HexaDecimal
+let hd=0xfd
+console.log(hd)
+
+//Infinity
+
+let num1=1/0
+console.log(num1)  //output:Infinity
+
+//Maximum Value
+console.log(Number.MAX_VALUE)
+
+//BigInt
+let bn=1050020154895205465n
+console.log(bn)
+console.log(bn+5n)
+
+//Boolean
+let b=5>6
+console.log(b)
--- Practice-KT/hello.js
+++ Practice-KT/hello.js
@@ -1,1 +1,39 @@
 console.log("hello World");
+console.log(2+2+3)
+let num =5;
+let num1 =6.5;
+let num2=num+num1;
+console.log(num2);
+//
+let username="Navin";
+console.log(username)
+if(username!=="Navin"){
+    console.log("Equal");
+}
+else{
+    console.log("Not Equal");
+}
+let username1="Reddy";
+
+let username2=username+username1;
+console.log(username2);
+console.log(username+username1);
+
+//Boolean and NUll value
+console.log(null);
+console.log(false)
+
+//Updating variable value
+username="Navin1";
+console.log(username);
+
+//Example
+let radius=5
+const pi=3.14
+let area
+area=pi*radius*radius
+console.log(area)
+//update radius value
+radius=6
+area=pi*radius*radius
+console.log(area)
